ec1ccacf3f611204966cd79696e9fa4071f0df55
ambro66 <124431818+ambro66@users.noreply.github.com>
2025-08-25 16:56:46 +0200

    h1

--- components/header.jsx
+++ components/header.jsx
@@ -1,15 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
 import netlifyLogo from "public/netlify-logo.svg";
-import githubLogo from "public/images/github-mark-white.svg";
 
 const navItems = [
-  { linkText: "Home", href: "/" },
-  { linkText: "Revalidation", href: "/revalidation" },
-  { linkText: "Image CDN", href: "/image-cdn" },
-  { linkText: "Edge Function", href: "/edge" },
-  { linkText: "Blobs", href: "/blobs" },
-  { linkText: "Classics", href: "/classics" },
   { linkText: "Pricing", href: "/pricing" },
   { linkText: "Login", href: "/login" },
   { linkText: "Register", href: "/register" },
@@ -26,7 +19,7 @@ export function Header() {
         </Link>
 
         {/* Menu po prawej */}
-        <ul className="hidden md:flex items-center gap-6 text-gray-700 font-medium">
+        <ul className="flex items-center gap-6 text-gray-700 font-medium">
           {navItems.map((item, index) => (
             <li key={index}>
               <Link
@@ -38,16 +31,6 @@ export function Header() {
             </li>
           ))}
         </ul>
-
-        {/* GitHub ikona po prawej stronie */}
-        <Link
-          href="https://github.com/netlify-templates/next-platform-starter"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="hidden md:inline-flex"
-        >
-          <Image src={githubLogo} alt="GitHub" width={28} height={28} />
-        </Link>
       </nav>
     </header>
   );
